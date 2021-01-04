a5716ba8aa27fd8510bd606e5ae69f3a9d4443fb
Raphael Debraize <raphaeldebraize@gmail.com>
2021-01-04 09:56:13 +0100

    Code JS de l'exercice 1

---
+++ scriptExo1.js
@@ -0,0 +1,8 @@
+function fonctionOver(){
+    document.getElementById("image1").src = "images/image1_2.jpg";
+   
+}
+
+function fonctionOut(){
+    document.getElementById("image1").src = "images/image1.jpg";
+}
