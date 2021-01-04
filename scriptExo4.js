fe345e31a22485e88f393cb8c2b12edffc1a6eb8
Raphael Debraize <raphaeldebraize@gmail.com>
2021-01-04 09:59:07 +0100

    Code JS de l'exercice 4

---
+++ scriptExo4.js
@@ -0,0 +1,9 @@
+let bouton = document.getElementById("btnRaz");
+
+
+bouton.addEventListener("click", (e) =>{
+     document.getElementById("lastname").value = "";
+     document.getElementById("firstname").value = "";
+     document.getElementById("city").value = "";
+
+});
