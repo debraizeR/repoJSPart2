d873edd642fcc65134665899a6161b3050beb900
Raphael Debraize <raphaeldebraize@gmail.com>
2021-01-04 09:59:40 +0100

    Code JS de l'exercice 6

---
+++ scriptExo6.js
@@ -0,0 +1,8 @@
+function changeImg(idImg){
+    document.getElementById(idImg).src = "images/"+idImg+"_2.jpg"
+}
+
+
+function changeMouseOut(idImg){
+    document.getElementById(idImg).src = "images/"+idImg+".jpg"
+}
